54c1694d827bcc5f33e8cc88ca670553df8958fd
priyanshu966106 <priyanshu966106@gmail.com>
2019-11-02 01:18:22 +0530

    feat: 🎸 Added install command

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const pjson = require('./package.json');
+const express = require("express");
 const inquirer  = require('./lib/inquirer');
-
+const si = require('systeminformation');
+app = express();
 
 clear();
 
@@ -16,10 +18,10 @@ console.log(
 
 console.log(
     chalk.yellow(
-      '\n 1 >> Setup \n 2 >> Add Database'
+      '\n 1 >> Setup \n 2 >> Add Database \n 3 >> System Information'
     )
   );
-
+  app.listen(3128);
 
 // const run = async () => {
 //     const credentials = await inquirer.askGithubCredentials();
